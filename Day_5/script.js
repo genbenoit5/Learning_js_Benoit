a7985e4251a1619ea8ff09abbbae28ab96b8aeee
genbenoit5 <genbenoit5@gmail.com>
2026-09-01 04:06:39 -0700

    declraed a count variable and assigned it to 0

--- Day_5/script.js
+++ Day_5/script.js
@@ -2,3 +2,5 @@ const countdisplay = document.getElementById("count");
 const decreasebtn = document.getElementById("decrease");
 const resetbtn = document.getElementById("reset");
 const increasebtn = document.getElementById("increase");
+
+let count = 0;
