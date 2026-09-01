e59d79aa2d90f3ae201ae31bc5247c569362ba88
genbenoit5 <genbenoit5@gmail.com>
2026-09-01 04:17:20 -0700

    added functionality to buttons

--- Day_5/script.js
+++ Day_5/script.js
@@ -4,3 +4,18 @@ const resetbtn = document.getElementById("reset");
 const increasebtn = document.getElementById("increase");
 
 let count = 0;
+
+decreasebtn.onclick = function(){
+    count--;
+    countdisplay.textContent = count;
+}
+
+increasebtn.onclick = function(){
+    count++;
+    countdisplay.textContent = count;
+}
+
+resetbtn.onclick = function(){
+    count = 0;
+    countdisplay.textContent = count;
+}
