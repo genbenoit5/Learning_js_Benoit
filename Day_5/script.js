1d5d8da8b971705a2756f45a395cd1e1f9e2274f
genbenoit5 <genbenoit5@gmail.com>
2026-09-01 04:28:30 -0700

    added an update function

--- Day_5/script.js
+++ Day_5/script.js
@@ -8,14 +8,29 @@ let count = 0;
 decreasebtn.onclick = function(){
     count--;
     countdisplay.textContent = count;
+     updatecolor()
 }
 
 increasebtn.onclick = function(){
     count++;
     countdisplay.textContent = count;
+     updatecolor()
 }
 
 resetbtn.onclick = function(){
     count = 0;
     countdisplay.textContent = count;
+     updatecolor()
+}
+
+function updatecolor(){
+    if(count < 0){
+        countdisplay.style.color = "red"
+    }
+    else if(count > 0){
+        countdisplay.style.color = "green"
+    }
+    else{
+        countdisplay.style.color = "black"
+    }
 }
