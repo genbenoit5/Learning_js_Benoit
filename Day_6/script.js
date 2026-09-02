b5800e32b07cf0950eb74a52a4c7e5e0c3376564
genbenoit5 <genbenoit5@gmail.com>
2026-09-02 03:19:02 -0700

    added a condition to calculate area of a circle and rectangle

--- Day_6/script.js
+++ Day_6/script.js
@@ -5,9 +5,36 @@ const triangleinputs = document.getElementById("triangleinputs");
 const calculatebtn = document.getElementById("calculatebtn");
 const result = document.getElementById("result");
 
-shape.addEventListener("change", function()){
+shape.addEventListener("change", function() {
 
     circleinputs.classList.add("hidden")
     rectangleinputs.classList.add("hidden")
     triangleinputs.classList.add("hidden")
-}
+
+    if (shape.value === "circle"){
+        circleinputs.classList.remove("hidden")
+    }
+    else if (shape.value === "rectangle"){
+        rectangleinputs.classList.remove("hidden")
+    }
+    else if (shape.value === "triangle"){
+        triangleinputs.classList.remove("hidden")
+    }
+});
+
+calculatebtn.addEventListener ("click", function () {
+
+    if (shape.value === "circle"){
+        const raduis = Number(document.getElementById("radius") .value);
+        const area = Math.PI * raduis * raduis;
+        result.textContent = `Area of circle = ${area.toFixed(2)} square units`;
+
+    } else if (shape.value === "rectangle"){
+        const length = Number(document.getElementById("length").value);
+        const width = Number(document.getElementById("width").value);
+        const area = length * width;
+        result.textContent = `Area of a rectangle = ${area.toFixed(2)} square units`;
+
+    }
+
+});
