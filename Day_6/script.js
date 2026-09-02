b9b5d29b59b75a949ae0ee8a14b3e65cf1d08bad
genbenoit5 <genbenoit5@gmail.com>
2026-09-01 22:30:46 -0700

    got elements from html

---
+++ Day_6/script.js
@@ -0,0 +1,6 @@
+const shape = document.getElementById("shape");
+const circleinputs = document.getElementById("circleinputs");
+const rectangleinputs = document.getElementById("rectangleinputs");
+const triangleinputs = document.getElementById("triangleinputs");
+const calculatebtn = document.getElementById("calculatebtn");
+const result = document.getElementById("result");
