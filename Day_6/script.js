78387514ade45c03149bbb573cf4739296f82b0c
genbenoit5 <genbenoit5@gmail.com>
2026-09-01 22:51:43 -0700

    included a function that detects change in shape and hides the other shapes

--- Day_6/script.js
+++ Day_6/script.js
@@ -4,3 +4,10 @@ const rectangleinputs = document.getElementById("rectangleinputs");
 const triangleinputs = document.getElementById("triangleinputs");
 const calculatebtn = document.getElementById("calculatebtn");
 const result = document.getElementById("result");
+
+shape.addEventListener("change", function()){
+
+    circleinputs.classList.add("hidden")
+    rectangleinputs.classList.add("hidden")
+    triangleinputs.classList.add("hidden")
+}
