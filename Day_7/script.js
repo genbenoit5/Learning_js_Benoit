65641cf980bee13edfcca5f732718f8984bdcb59
genbenoit5 <genbenoit5@gmail.com>
2026-09-03 20:31:15 -0700

    get html elements

--- Day_7/script.js
+++ Day_7/script.js
@@ -1,9 +1,15 @@
 let secretNumber;
-
 let attemptsLeft;
-
 let score;
-
 let bestScore = 0;
-
 let previousGuesses = [];
+
+
+const guessinput = document.getElementById("guessinput");
+const guessbtn = document.getElementById("guessbtn");
+const message = document.getElementById("message");
+const attemptsdisplay = document.getElementById("attempts");
+const scoredisplay = document.getElementById("score");
+const bestscoredisplay = document.getElementById("bestscore");
+const guesslist = document.getElementById("guesslist");
+const newgamebtn = document.getElementById("newgamebtn");
